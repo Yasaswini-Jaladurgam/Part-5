d4e5d40d10f2946dd80e2945f82e80481846b852
Yasaswini-Jaladurgam <jyashjaladurgam@gmail.com>
2021-06-04 15:50:58 +0530

    Add files via upload

---
+++ bloglist-frontend/src/App.js
@@ -0,0 +1,139 @@
+import React, { useState, useEffect } from 'react'
+import  { useField } from './hooks'
+import Heading from './components/Heading'
+import CurrentUser from './components/CurrentUser'
+import LoginForm from './components/LoginForm'
+import BlogForm from './components/BlogForm'
+import Blogs from './components/Blogs'
+import Togglable from './components/Togglable'
+import blogService from './services/blogs'
+import loginService from './services/login'
+
+const App = () => {
+ 
+  const [blogs, setBlogs] = useState([])
+  const [users, setUsers] = useState([])
+  const [user, setUser] = useState(null)
+  const [message, setMessage] = useState(null)
+  const blogFormRef = React.createRef()
+  useEffect(() => {
+    blogService.getAllUsers().then(users => { setUsers(users) })
+    blogService.getAll().then(initialBlogs => { setBlogs(initialBlogs) })
+
+    const loggedUserJSON = window.localStorage.getItem('loggedBlogappUser')
+    if (loggedUserJSON) {
+      const user = JSON.parse(loggedUserJSON)
+      setUser(user)
+      blogService.setToken(user.token)
+    }
+  }, [])
+
+  const username = useField('text', 'Username')
+  const password = useField('password', 'Password')
+  const title = useField('text', 'Title')
+  const author = useField('text', 'Author')
+  const url = useField('text', 'Url')
+  const handleLogin = async (event) => {
+    event.preventDefault()
+    try {
+      const user = await loginService.login({
+        username: username.value,
+        password: password.value
+      })
+      window.localStorage.setItem(
+        'loggedBlogappUser', JSON.stringify(user)
+      )
+      setUser(user)
+      blogService.setToken(user.token)
+      username.reset()
+      password.reset()
+    } catch (exception) {
+      setMessage('Wrong username or password')
+      setTimeout(() => {
+        setMessage(null)
+      }, 5000)
+    }
+  }
+
+  const handleLogout = async (event) => {
+    event.preventDefault()
+    window.localStorage.clear()
+    setUser(null)
+  }
+
+  const addBlog = (event) => {
+    event.preventDefault()
+    blogFormRef.current.toggleVisibility()
+    const blogObject = {
+      title: title.value,
+      author: author.value,
+      url: url.value,
+    }
+
+    blogService
+      .create(blogObject)
+      .then(data => {
+        setMessage(`a new blog ${title.value} by ${author.value} added`)
+        setTimeout(() => {
+          setMessage(null)
+        }, 5000)
+        setBlogs(blogs.concat(data))
+      })
+    title.reset()
+    author.reset()
+    url.reset()
+
+  }
+
+  const handleLikes = (event) => {
+    event.preventDefault()
+    const id = event.target.value
+    const blog = blogs.find(n => n.id === id)
+    const changedBlog = {
+      title: blog.title,
+      author: blog.author,
+      url: blog.url,
+      likes: blog.likes + 1,
+      user: blog.user.id || blog.user 
+    }
+    blogService
+      .update(id, changedBlog)
+      .then(returnedBlog => {
+        setBlogs(blogs.map(blog => blog.id !== id ? blog : returnedBlog))
+      })
+  }
+
+  const handleRemove = (event) => {
+    event.preventDefault()
+    const id = event.target.value
+    const blog = blogs.find(n => n.id === id)
+    if (window.confirm(`remove blog ${blog.title} by ${blog.author}`)) {
+      blogService.remove(id)
+        .catch(error => {
+          setBlogs(blogs.filter(n => n.id !== id))
+        })
+      setBlogs(blogs.filter(n => n.id !== id))
+    }
+  }
+
+  return (
+    <div>
+      {user === null ?
+        <div>
+          <Heading text={'log in to application'} message={message} />
+          <LoginForm handleLogin={handleLogin} username={username} password={password} />
+        </div> :
+        <div>
+          <Heading text={'blogs'} message={message} />
+          <CurrentUser user={user} handleLogout={handleLogout} />
+          <Togglable buttonLabel="new blog" ref={blogFormRef}>
+            <BlogForm handleAdding={addBlog} title={title} author={author} url={url} />
+          </Togglable>
+          <Blogs blogs={blogs} users={users} handleLikes={handleLikes} handleRemove={handleRemove} />
+        </div>
+      }
+    </div>
+  )
+}
+
+export default App
---
+++ bloglist-frontend/src/App.test.js
@@ -0,0 +1,43 @@
+import React from 'react'
+import { render, waitForElement } from '@testing-library/react'
+jest.mock('./services/blogs')
+import App from './App'
+
+describe('<App />', () => {
+  test('if no user logged, blogs are not rendered', async () => {
+    const component = render(
+      <App />
+    )
+    component.rerender(<App />)
+
+    await waitForElement(
+      () => component.getByText('login')
+    )
+
+    expect(component.container).toHaveTextContent('log in to applicationusernamepasswordlogin')
+    expect(component.container).not.toHaveTextContent('blogs')
+  })
+
+  test('if user is logged in, the blog posts are rendered to the page', async () => {
+    const user = {
+      username: 'tester',
+      token: '1231231214',
+      name: 'Donald Tester'
+    }
+    await localStorage.setItem('loggedBlogappUser', JSON.stringify(user))
+
+    const component = render(
+      <App />
+    )
+
+    component.rerender(<App />)
+
+    await waitForElement(
+      () => component.getByText('blogs')
+    )
+
+    
+    expect(component.container).not.toHaveTextContent('log in to applicationusernamepasswordlogin')
+    expect(component.container).toHaveTextContent('blogs')
+  })
+})
